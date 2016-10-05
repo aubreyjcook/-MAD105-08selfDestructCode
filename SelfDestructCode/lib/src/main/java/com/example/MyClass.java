e7ce598f9845a22e16ff2ed416e088ddd0fc913a
Aubrey Cook <aubreyjcook@gmail.com>
2016-10-04 21:02:37 -0500

    complete

--- SelfDestructCode/lib/src/main/java/com/example/MyClass.java
+++ SelfDestructCode/lib/src/main/java/com/example/MyClass.java
@@ -1,6 +1,43 @@
 package com.example;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
+
 public class MyClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        boolean foundCode = false;
+        String[] validCodes = new String[]{"119bY7", "84T642", "K8711v"};
+        int codeCountProtocol = 3;
+
+
+        do {
+            Scanner userInput = new Scanner(System.in);
+            System.out.println("Enter a valid code to verify the self destruct sequence:");
+            String userCode = userInput.next();
+
+            if (Arrays.asList(validCodes).contains(userCode)) {
+                foundCode = true;
+                System.out.println("Valid code confirmed.");
+                if (userCode.equals("119bY7")) {
+                    validCodes[0] = "";
+                } else if (userCode.equals("84T642")) {
+                    validCodes[1] = "";
+                } else if (userCode.equals("K8711v")) {
+                    validCodes[2] = "";
+                }
+
+            } else {
+                System.out.println("Valid code not found.");
+            }
+
+            if (foundCode) {
+                codeCountProtocol--;
+                foundCode = false;
+            }
+        } while (codeCountProtocol > 0);
+
+        System.out.println("Self-destruct code protocol verified, commencing self-destruct sequence.");
+
     }
 }
